4c301cfe1cdefbceeaa36479bd666439ca227068
Declan Brown <135834907+DeclanKBrown@users.noreply.github.com>
2023-07-29 02:13:21 +1000

    Fixed CSS when resizing

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Library
+
+- Save labrary to local storage
+- On page load iterate through the loop displaying all books
+- Display only the added book on submit
+- Add read/reading/not read yet color function
+<!-- - fix CSS resizing  -->
+- Autofut css
+- Light Dark mode toggle
+- Clear values from add new book
--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 let Library = [];
 
 function Book(title, author, pages, file) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
     this.file = file;
 }
 
 const addbtn = document.querySelector('.add');
-addbtn.addEventListener('click', showBook);
+addbtn.addEventListener('click', addBook);
 
 let title;
 let author;
 let pages;
 let filePath;
 
-
+// Displays Cover in Add Book Pop up
 const addImage = document.querySelector('.addImage');
 const bookCover = document.querySelector('.cover-input');
 addImage.addEventListener('change', function() {
@@ -26,18 +26,20 @@ addImage.addEventListener('change', function() {
     }
 })
 
+// Add Book 
 const submit = document.querySelector('.submit');
 submit.addEventListener('click', function() {
-    //GetValues from input 
-    MainTitle = titleInput.value;
-    MainAuthor = authorInput.value;
-    MainPages = pagesInput.value;
+    //Get Values from input 
+    MainTitle = document.querySelector('.title-input').value;
+    MainAuthor = document.querySelector('.author-input').value;
+    MainPages = document.querySelector('.pages-input').value;
     const selectedFile = addImage.files[0];
     MainFilePath = URL.createObjectURL(selectedFile);
     //Create Object
-    let book = Book(MainTitle, MainAuthor, MainPages, MainFilePath);
+    let book = new Book(MainTitle, MainAuthor, MainPages, MainFilePath);
     //Add to array
     Library.push(book);
+    displayBook();
     removePopup();
 })
 
@@ -46,11 +48,51 @@ cancel.addEventListener('click', function() {
     removePopup();
 })
 
-function showBook() {
+function addBook() {
     document.getElementById('modal').classList.add('overlay')
     document.getElementById('modal').style = 'display:grid;'
 }
 
 function removePopup() {
     document.getElementById('modal').style = 'display:none;'
+}
+
+function displayBook() {
+    for (book in Library) {
+        const card = document.createElement('div');
+        card.classList.add('book');
+    
+        const top = document.createElement('div');
+        top.classList.add('top');
+    
+        const cover = document.createElement('div');
+        cover.classList.add('cover');
+    
+        const img = document.createElement('img');
+        img.src = Library[book].file;
+    
+        const status = document.createElement('div');
+        status.classList.add('status');
+        status.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>moon-full</title><path d="M12 2A10 10 0 1 1 2 12A10 10 0 0 1 12 2Z" /></svg>';
+    
+        const h1 = document.createElement('h1');
+        h1.innerHTML = Library[book].title
+    
+        const h2 = document.createElement('h2');
+        h2.innerHTML = Library[book].author;
+    
+        const h3 = document.createElement('h3');
+        h3.innerHTML = Library[book].pages;
+    
+        card.appendChild(top);
+        top.appendChild(cover);
+        cover.appendChild(img);
+        top.appendChild(status);
+        card.appendChild(h1);
+        card.appendChild(h2);
+        card.appendChild(h3);
+    
+        const inner = document.querySelector('.book-display-inner');
+        inner.appendChild(card);
+    }
 }
