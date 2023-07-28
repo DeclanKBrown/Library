0abd556e9c300c4ed110c69a9c451aadd96ed714
Declan Brown <135834907+DeclanKBrown@users.noreply.github.com>
2023-07-29 00:08:48 +1000

    Added add book function

--- script.js
+++ script.js
@@ -1,116 +1,56 @@
 let Library = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, file) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.file = file;
 }
 
 const addbtn = document.querySelector('.add');
-addbtn.addEventListener('click', addBook);
+addbtn.addEventListener('click', showBook);
 
-let MainTitle;
-let MainAuthor;
-let MainPages;
-let MainFilePath;
+let title;
+let author;
+let pages;
+let filePath;
 
-function addBook() {
-    const overlay = document.createElement('div');
-    overlay.classList.add('overlay');
 
-    const popup = document.createElement('div');
-    popup.classList.add('book-input');
+const addImage = document.querySelector('.addImage');
+const bookCover = document.querySelector('.cover-input');
+addImage.addEventListener('change', function() {
+    const selectedFile = addImage.files[0];
+    if (selectedFile) {
+        const filePath = URL.createObjectURL(selectedFile);
+        bookCover.src = filePath;
+    }
+})
 
-    const popupInner = document.createElement('div');
-    popupInner.classList.add('popup-inner');
+const submit = document.querySelector('.submit');
+submit.addEventListener('click', function() {
+    //GetValues from input 
+    MainTitle = titleInput.value;
+    MainAuthor = authorInput.value;
+    MainPages = pagesInput.value;
+    const selectedFile = addImage.files[0];
+    MainFilePath = URL.createObjectURL(selectedFile);
+    //Create Object
+    let book = Book(MainTitle, MainAuthor, MainPages, MainFilePath);
+    //Add to array
+    Library.push(book);
+    removePopup();
+})
 
-    const addTitle = document.createElement('h1');
-    addTitle.classList.add('add-title');
-    addTitle.innerHTML = 'Add Book';
+const cancel = document.querySelector('.cancel');
+cancel.addEventListener('click', function() {
+    removePopup();
+})
 
-    const bookCover = document.createElement('img')
-    bookCover.classList.add('cover-input')
+function showBook() {
+    document.getElementById('modal').classList.add('overlay')
+    document.getElementById('modal').style = 'display:grid;'
+}
 
-    const addImage = document.createElement('input');
-    addImage.type = 'file';
-    addImage.innerHTML = 'Add Cover';
-
-    const bookInfo = document.createElement('div');
-    bookInfo.classList.add('book-info');
-
-    const title = document.createElement('span');
-    title.classList.add('info-input');
-    title.innerHTML = 'Title:';
-
-    const titleInput = document.createElement('input')
-    titleInput.type = 'text';
-    titleInput.classList.add('TAP');
-
-    const author = document.createElement('span');
-    author.classList.add('info-input');
-    author.innerHTML = 'Author';
-
-    const authorInput = document.createElement('input')
-    authorInput.type = 'text';
-    authorInput.classList.add('TAP');
-
-    const pages = document.createElement('span');
-    pages.classList.add('info-input');
-    pages.innerHTML = 'Pages';
-
-    const pagesInput = document.createElement('input')
-    pagesInput.type = 'text';
-    pagesInput.classList.add('TAP');
-
-    const scbtn = document.createElement('div');
-    scbtn.classList.add('sc-btn');
-
-    const cancel = document.createElement('Button');
-    cancel.innerHTML = 'Cancel';
-    cancel.classList.add('sc-btns');
-    cancel.classList.add('cancel');
-
-    const submit = document.createElement('Button');
-    submit.innerHTML = 'Add';
-    submit.classList.add('sc-btns');
-    submit.classList.add('Submit');
-
-    document.body.appendChild(overlay);
-    overlay.appendChild(popup);
-    popup.appendChild(popupInner);
-    popupInner.appendChild(addTitle);
-    popupInner.appendChild(bookCover);
-    popupInner.appendChild(addImage);
-    popupInner.appendChild(bookInfo)
-    bookInfo.appendChild(title);
-    bookInfo.appendChild(titleInput);
-    bookInfo.appendChild(author);
-    bookInfo.appendChild(authorInput);
-    bookInfo.appendChild(pages);
-    bookInfo.appendChild(pagesInput);
-    popupInner.appendChild(scbtn);
-    scbtn.appendChild(cancel);
-    scbtn.appendChild(submit);
-
-    addImage.addEventListener('change', function() {
-        const selectedFile = addImage.files[0];
-        if (selectedFile) {
-            const filePath = URL.createObjectURL(selectedFile);
-            bookCover.src = filePath;
-        }
-    })
-
-    submit.addEventListener('click', function() {
-        MainTitle = titleInput.value;
-        MainAuthor = authorInput.value;
-        MainPages = pagesInput.value;
-        const selectedFile = addImage.files[0];
-        MainFilePath = URL.createObjectURL(selectedFile);
-        removePopup();
-    })
-
-    cancel.addEventListener('click', function() {
-        removePopup();
-    })
+function removePopup() {
+    document.getElementById('modal').style = 'display:none;'
 }
